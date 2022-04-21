676e3dbcad6efca55c50ca19300abbcd745c6789
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-04-21 11:58:38 +0300

    changed to leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const App = () => {
   const location = useGetCurrentLocation();
-  const { find } = useAppSelector(state => state.appSlice.input);
+  const { find } = useAppSelector((state) => state.appSlice.input);
 
   const dispatch = useAppDispatch();
   console.log(location);
@@ -18,47 +18,31 @@ const App = () => {
     dispatch(setLocation(location));
   }, [location]);
 
-  if (location.isLoading) {
-    return <h1>LOADING...</h1>;
-  }
+  return (
+    <div className="app">
+      <div className="app__interface">interface</div>
 
-  if (location.error.status) {
-    return <h1>Some error occure</h1>;
-  }
-
-  if (location.coordinates.lat && location.coordinates.lng) {
-    return (
-      <div className='app'>
-        <div className="app__interface">
-          aside
-          <input 
-            type="text" 
-            value={find}
-            onChange={(event) => {
-              dispatch(inputHandler(event.target.value))
-            }}
-          />
-        </div>
-        <MapContainer
-          center={[location.coordinates.lat, location.coordinates.lng]}
-          zoom={13}
-          scrollWheelZoom={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <div className="leaflet-container">
+        {location.coordinates.lat && location.coordinates.lng && (
+          <MapContainer
+            center={[location.coordinates.lat, location.coordinates.lng]}
+            zoom={13}
+            scrollWheelZoom={true}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        )}
       </div>
-    );
-  }
-
-  return null;
+    </div>
+  );
 };
 
 export default App;

5ee159d5caf3342d6c6346bb69cb46642b9aa3ec
Osypiuk Dmytro <dimich578ua@gmail.com>
2022-04-21 11:51:57 +0300

    pre change (move back to leaflet)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react';
 import useGetCurrentLocation from './hooks/useGetCurrentLocation';
 import { setLocation } from './redux/appSlice';
 import { useDispatch } from 'react-redux';
-import Map from './components/Map';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const App = () => {
   const location = useGetCurrentLocation();
@@ -29,7 +29,21 @@ const App = () => {
     <div className="app">
       <div className="app__interface">interface</div>
       <div className="map__container">
-        <Map coordinates={location.coordinates}/>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
